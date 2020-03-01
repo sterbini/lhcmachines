9744ab3a19a71ab130409a6759f2fcdf38790c46
Guido Sterbini <guido.sterbini@cern.ch>
2020-03-01 16:52:50 +0100

    Links modified to files (EOS compatibility https://github.com/lhcopt/lhcmask/issues/9)

---
+++ optics/copy_files.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+cp /afs/cern.ch/eng/lhc/optics/HLLHCV1.4/round/opt_round_150_1500_thin.madx hl14_collision.madx
+cp /afs/cern.ch/eng/lhc/optics/HLLHCV1.4/ramp/opt_inj_6000_thin.madx hl14_injection.madx
